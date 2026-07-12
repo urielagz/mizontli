bc57bd718f6095772c240ae7b87b2076f1519c7a
ASRM65 <25308206@utcancun.edu.mx>
2026-07-12 14:48:20 -0500

    Update registro.js

--- por/js/registro.js
+++ por/js/registro.js
@@ -1,10 +1,17 @@
 function mostrarPDF() {
     const rol = document.getElementById("rol").value;
     const pdf = document.getElementById("pdf");
+    const submitButton = document.getElementById("submitButton");
+    const contraseña = document.getElementById("contraseña");
 
     if (rol === "Maestro") {
         pdf.style.display = "block";
-    } else {
+        submitButton.style.display = "block";
+        contraseña.style.display = "none";
+    } 
+    else {
         pdf.style.display = "none";
+        submitButton.style.display = "none";
+        contraseña.style.display = "block";
     }
 }
